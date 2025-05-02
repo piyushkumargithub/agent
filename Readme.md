edde35b0de20fe77a374c46b8a5e7a37a21a3b41
Piyush <piyushk123umar@gmail.com>
2025-05-02 23:10:48 +0530

    added readme file

---
+++ Readme.md
@@ -0,0 +1,28 @@
+# Agent Program
+
+## Overview
+An agent using smolagent by huggingface
+
+### Prerequisites
+- Python 3.8 or later must be installed on your system.
+- Ensure `pip` is installed and updated to the latest version.
+
+### Steps
+1. Clone the repository to your local machine:
+   ```bash
+   git clone https://github.com/piyushkumargithub/agent.git
+   cd agent
+   ```
+2. Create a virtual environment:
+    ```bash 
+    python -m venv agentenv
+    ```
+3. Activate the virtual environment:
+    ```bash
+    agentenv\Scripts\activate
+    ```
+4. Install dependencies:
+    ```bash
+    pip install -r requirements.txt
+    ```
+    
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
-from smolagents import OpenAIServerModel, CodeAgent
+from smolagents import OpenAIServerModel, CodeAgent,DuckDuckGoSearchTool
 
 model = OpenAIServerModel(
     model_id="gpt-4o-mini",
@@ -11,9 +11,11 @@ model = OpenAIServerModel(
 
 agent = CodeAgent(
     model=model,
-    tools=[]
+    tools=[DuckDuckGoSearchTool()],
+    description="Runs web searches for you. Give it your query as an argument.",
+    planning_interval=3
 )
 
 agent.run(
-    "Write a fibonnaci function in python"
+    "Search for what is capital of India and then find all the popular places there then all the best food items in each of those places."
 )
